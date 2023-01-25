689924f21371134518eb82c7aec38ac0a18f1cb9
Yosri-ctrl <yosribouabid@gmail.com>
2023-01-25 17:58:34 +0100

    implementing typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+
+import * as path from 'path';
+const baseDir = path.join(__dirname, '../');
+const entitiesPath = `${baseDir}${process.env.TYPEORM_ENTITIES}`;
+const migrationPath = `${baseDir}${process.env.TYPEORM_MIGRATIONS}`;
 
 @Module({
   imports: [TasksModule],
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TasksController],
   providers: [TasksService],
 })
