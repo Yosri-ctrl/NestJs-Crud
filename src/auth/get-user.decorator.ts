49e037feda15abd0a923cd362a7a9b76ff8e6001
Yosri-ctrl <yosribouabid@gmail.com>
2023-01-28 22:13:32 +0100

    fixing abug in user decorator

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -2,6 +2,6 @@ import { createParamDecorator } from '@nestjs/common';
 import { User } from './user.entity';
 
 export const GetUser = createParamDecorator((_data, ctx): User => {
-  const req = ctx.switchToHttp.getRequest();
+  const req = ctx.switchToHttp().getRequest();
   return req.user;
 });
