fd3c4c1179bddbb0ffaf9a16aa770043de0e6ffd
Yosri-ctrl <yosribouabid@gmail.com>
2023-02-01 20:52:26 +0100

    updating tests for getTaskById:

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,15 +1,26 @@
 import { TaskRepository } from './task.repository';
 import { TasksService } from './tasks.service';
 import { Test } from '@nestjs/testing';
+import { User } from 'src/auth/user.entity';
+import { Task } from './task.entity';
+import { taskStatus } from './task-status.enum';
 
 const mockTaskRepository = () => ({
   getAllTasks: jest.fn(),
+  getTaskById: jest.fn(),
 });
-const mockUser = {
+const mockUser: User = {
   username: 'John',
   password: '123456',
   id: '1',
   task: null,
+};
+const mockTask: Task = {
+  title: 'title',
+  description: 'description',
+  id: '2',
+  user: mockUser,
+  status: taskStatus.OPEN,
 };
 
 describe('Tasks service', () => {
@@ -39,6 +50,17 @@ describe('Tasks service', () => {
       const result = await taskService.getAllTasks(null, mockUser);
       expect(taskRepository.getAllTasks).toHaveBeenCalled();
       expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls taskRepository.getTaskById and return a task with that ID', async () => {
+      expect(taskRepository.getTaskById).not.toHaveBeenCalled();
+
+      taskRepository.getTaskById.mockResolvedValue(mockTask);
+      const result = await taskService.getTaskById(mockTask.id, mockUser);
+      expect(taskRepository.getTaskById).toHaveBeenCalled();
+      expect(result).toEqual(mockTask);
     });
   });
 });
